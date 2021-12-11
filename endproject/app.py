c8c9070673f1465c24064fa62e92df96f28a55d8
Mayuresh123 <mayureshkhanaj@gmail.com>
2021-12-11 12:15:00 +0530

    changed app.py and included streamlit in it,app ready to deploy

--- endproject/app.py
+++ endproject/app.py
@@ -1,19 +1,27 @@
+
+
 from model import algo
 import pickle
+import streamlit as st
 
 # load pickle file
 with open('model.pickle', 'rb') as handle:
     model = pickle.load(handle)
 with open('trained_model.pickle', 'rb') as handle:
     trained_model = pickle.load(handle)
+pred_model = algo( model = model , trained_model = trained_model )
 
 def prediction():
-    pred_model = algo( model = model , trained_model = trained_model )
-    article = open( "article.txt" , 'r',encoding = "utf-8" )
-    article = article.read()
-    print(article)
-    pred_val = pred_model.predict_tag(article).upper()
-    print("\nThis article is about", pred_val)
+    st.title('BBC News Article Classification')
+    st.write('This is a simple web app that predicts the category of an article based on the text of the article.')
+    st.write('The model is trained on the BBC News dataset and is based on MLP classifier.')
+
+    article  = st.text_input('Enter the article text:')
+
+
+    if st.button('Predict'):
+        pred_val = pred_model.predict_tag(article).upper()
+        st.success('The article is classified as {}'.format( pred_val))
 
 if __name__ == '__main__':
     prediction()
